dc26529471686e34005408e15c7024aa67500964
conni <cbuschka@gmail.com>
2022-01-04 19:59:29 +0100

    Added a class and instance creation.

--- hello.rs
+++ hello.rs
@@ -1,3 +1,20 @@
+pub struct Sayer {
+    message: String
+}
+
+impl Sayer {
+    pub fn new(message: String) -> Self {
+        Sayer { message }
+    }
+    pub fn say(&self) -> String {
+        return self.message.clone();
+    }
+
+}
+
 fn main() {
-    println!("Hello World!");
+    let sayer = Sayer::new("Hello World!".to_string());
+    let message = sayer.say();
+
+    println!("{}", message);
 }
