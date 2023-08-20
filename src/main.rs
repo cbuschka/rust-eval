d5ff573e74a30d2ae0343eb556eaf64e1b53fc55
conni <cbuschka@gmail.com>
2023-08-20 08:16:32 +0200

    Added json serialization.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use serde::{Deserialize, Serialize};
+use serde_json::Result;
+
+#[derive(Serialize, Deserialize)]
+struct Message {
+    text: String,
+}
+
+
+fn print_hello() -> Result<()> {
+    let message = Message {
+        text: "Hello world".to_owned(),
+    };
+
+    let json = serde_json::to_string(&message)?;
+
+    println!("{}", json);
+
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    let _ = print_hello();
 }
